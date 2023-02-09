58b861bac6afb2a8042c61efd0e2af5bd07f7c3b
Shridhar S D <shridhard@gmail.com>
2023-02-09 11:38:17 +0530

    using GenericUtility1.getRandomNumber

--- src/test/java/knt/lib/generic1/TestSecurityHelper1.java
+++ src/test/java/knt/lib/generic1/TestSecurityHelper1.java
@@ -15,27 +15,32 @@ import javax.crypto.Cipher;
 import org.junit.jupiter.api.Test;
 
 public class TestSecurityHelper1 {
-
-    public ArrayList<Integer> getRandomArray(int min, int max, int count) {
-        ArrayList<Integer> inputArray = new ArrayList<>();
-
-        for ( int counterr = 0; counterr < count; counterr++) {
-            inputArray.add((int)Math.random() * (max - min + 1 + counterr) + min);
-        }
-        return inputArray;
+    // TestSecurityHelper1#testgetRandomNumberValid
+    @Test
+    public void testgetRandomNumberValid() {
+        int outputVar;
+        int minInt = 1;
+        int maxInt = 129;
+        outputVar = GenericUtility1.getRandomNumber(minInt, maxInt);
+        System.out.println("**************************");
+        System.out.println("securityHelper1:getRandomArray: " + outputVar);
+        System.out.println("**************************");
+        assertTrue(outputVar > minInt && outputVar < maxInt);
     }
+
     @Test
     public void testGetRandomArray() {
         ArrayList<Integer> outputVar;
-        outputVar = getRandomArray(1, 129, 6);
+        outputVar = GenericUtility1.getRandomArray(1, 129, 6);
         System.out.println("securityHelper1:getRandomArray: " + outputVar);
         assertTrue(outputVar != null);
     }
+
     public void testGetUniformNumberString() {
         SecurityHelper1 securityHelper1 = new SecurityHelper1("content1", "passCode1");
-//        ArrayList<Integer> inputArray2 = new int[]{10, 1, 9, 122, 292, 8};
+        // ArrayList<Integer> inputArray2 = new int[]{10, 1, 9, 122, 292, 8};
         ArrayList<Integer> inputArray = new ArrayList<>();
-        for ( int inputItem: new int[]{10, 1, 9, 122, 292, 8} ) {
+        for (int inputItem : new int[] { 10, 1, 9, 122, 292, 8 }) {
             inputArray.add(inputItem);
         }
         securityHelper1.getUniformNumberString(inputArray);
@@ -43,6 +48,7 @@ public class TestSecurityHelper1 {
         System.out.println("securityHelper1:contentEn: " + outputVar);
         assertTrue(outputVar != null);
     }
+
     public void testCodesEn() {
         String content = "TestInput";
         System.out.println("securityHelper1:testCodesEn:content: " + content);
@@ -51,11 +57,12 @@ public class TestSecurityHelper1 {
         System.out.println("securityHelper1:testCodesEn:contentEn: " + securityHelper1.getContentEn());
         assertTrue(securityHelper1.getContentEn() != null);
         /*
-        Input:content:  TestInput
-        content.charCodeAt(i):  84  # passcodeLocal.charCodeAt(passOffset):  36
-        calAscii:  120
+         * Input:content: TestInput
+         * content.charCodeAt(i): 84 # passcodeLocal.charCodeAt(passOffset): 36
+         * calAscii: 120
          */
     }
+
     public void testCodesDe() {
         String content = "TestInput";
         System.out.println("securityHelper1:testCodesEn:content: " + content);
@@ -68,47 +75,49 @@ public class TestSecurityHelper1 {
         assertTrue(securityHelper1.getContent() != null);
 
     }
+
     public String encrypt(String inputStr) {
         try {
             // (String args[]) throws Exception
-            //Creating a Signature object
+            // Creating a Signature object
             Signature sign = Signature.getInstance("SHA256withRSA");
 
-            //Creating KeyPair generator object
+            // Creating KeyPair generator object
             KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
 
-            //Initializing the key pair generator
+            // Initializing the key pair generator
             keyPairGen.initialize(2048);
 
-            //Generate the pair of keys
+            // Generate the pair of keys
             KeyPair pair = keyPairGen.generateKeyPair();
 
-            //Getting the public key from the key pair
-//            commented to test RSAPublicKey
-//            PublicKey publicKeyOri = pair.getPublic();
+            // Getting the public key from the key pair
+            // commented to test RSAPublicKey
+            // PublicKey publicKeyOri = pair.getPublic();
 
-//            Approach changes / TODOs:
-//            Create a KeyPair on first signin or use
-//            serialize and store it as key values for future use
-//            Each SMS is then encrypted and saved.
-//            While consumption and segregation use decrypt and use
+            // Approach changes / TODOs:
+            // Create a KeyPair on first signin or use
+            // serialize and store it as key values for future use
+            // Each SMS is then encrypted and saved.
+            // While consumption and segregation use decrypt and use
 
-//            Logic source : https://stackoverflow.com/questions/21606428/serialize-and-deserialize-an-rsa-public-key
-            RSAPublicKey publicKey = (RSAPublicKey)pair.getPublic();
-            String serializedModulusStr =  publicKey.getModulus().toString();
+            // Logic source :
+            // https://stackoverflow.com/questions/21606428/serialize-and-deserialize-an-rsa-public-key
+            RSAPublicKey publicKey = (RSAPublicKey) pair.getPublic();
+            String serializedModulusStr = publicKey.getModulus().toString();
             String serializedpubExpoStr = publicKey.getPublicExponent().toString();
 
-            //Creating a Cipher object
+            // Creating a Cipher object
             Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
-            //Initializing a Cipher object
+            // Initializing a Cipher object
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
 
-            //Add data to the cipher
+            // Add data to the cipher
             byte[] input = inputStr.getBytes();
             cipher.update(input);
 
-            //encrypting the data
+            // encrypting the data
             byte[] cipherText = cipher.doFinal();
             String outputStr = new String(cipherText, "UTF8");
             System.out.println("Output Str: " + outputStr);
@@ -118,56 +127,57 @@ public class TestSecurityHelper1 {
             return e.getMessage();
         }
     }
+
     public void testCodesEnDe() {
         try {
             // (String args[]) throws Exception
-            //Creating a Signature object
+            // Creating a Signature object
             Signature sign = Signature.getInstance("SHA256withRSA");
 
-            //Creating KeyPair generator object
+            // Creating KeyPair generator object
             KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
 
-            //Initializing the key pair generator
+            // Initializing the key pair generator
             keyPairGen.initialize(2048);
 
-            //Generate the pair of keys
+            // Generate the pair of keys
             KeyPair pair = keyPairGen.generateKeyPair();
 
-            //Getting the public key from the key pair
+            // Getting the public key from the key pair
             PublicKey publicKey = pair.getPublic();
 
-            //Creating a Cipher object
+            // Creating a Cipher object
             Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
-            //Initializing a Cipher object
+            // Initializing a Cipher object
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
 
-            //Add data to the cipher
+            // Add data to the cipher
             byte[] input = "Welcome to Tutorialspoint".getBytes();
             cipher.update(input);
 
-            //encrypting the data
+            // encrypting the data
             byte[] cipherText = cipher.doFinal();
-            System.out.println( new String(cipherText, "UTF8"));
+            System.out.println(new String(cipherText, "UTF8"));
 
-            //Initializing the same cipher for decryption
+            // Initializing the same cipher for decryption
             cipher.init(Cipher.DECRYPT_MODE, pair.getPrivate());
 
-            //Decrypting the text
+            // Decrypting the text
             byte[] decipheredText = cipher.doFinal(cipherText);
             System.out.println(new String(decipheredText));
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
     public void test_generateStoreKeys1() {
-//        generateStoreKeys
+        // generateStoreKeys
         final String METHOD_NAME = "test_generateStoreKeys1:";
         String content = "TestInput";
         String algorithm = "RSA";
-        System.out.println(METHOD_NAME + "securityHelper1:testCodesEn:content: " + content + " # algorithm: " + algorithm);
+        System.out.println(
+                METHOD_NAME + "securityHelper1:testCodesEn:content: " + content + " # algorithm: " + algorithm);
         SecurityHelper1 securityHelper1 = new SecurityHelper1(content, null);
         KeyPair keyPair = securityHelper1.generateStoreKeys(algorithm);
         byte[] privateKeyBytes;
@@ -175,17 +185,17 @@ public class TestSecurityHelper1 {
 
         try {
             PublicKey publicKey = keyPair.getPublic();
-            //Creating a Cipher object
+            // Creating a Cipher object
             Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
-            //Initializing a Cipher object
+            // Initializing a Cipher object
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
 
-            //Add data to the cipher
+            // Add data to the cipher
             byte[] input = securityHelper1.getContent().getBytes();
             cipher.update(input);
 
-            //encrypting the data
+            // encrypting the data
             byte[] cipherText = cipher.doFinal();
             System.out.println(METHOD_NAME + "cipherText: " + new String(cipherText, "UTF8"));
 
@@ -193,70 +203,75 @@ public class TestSecurityHelper1 {
             PrivateKey privateKey = securityHelper1.deSerialize(algorithm, privateKeyBytes, null);
 
             cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-            //Initializing the same cipher for decryption
+            // Initializing the same cipher for decryption
             cipher.init(Cipher.DECRYPT_MODE, privateKey);
 
-            //Decrypting the text
+            // Decrypting the text
             byte[] decipheredText = cipher.doFinal(cipherText);
             System.out.println(METHOD_NAME + "decipheredText: " + new String(decipheredText));
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-
     }
-    //    public void testEncryDecry() {
-//        String content = "TestInput";
-//        String decryptedStr;
-//        System.out.println("securityHelper1:testCodesEn:content: " + content);
-//        try {
-//            String passCode = "$W%Ld0os$3$Wdos3";// String key = "Bar12345Bar12345"; // 128 bit key
-//            String encryptedStr = encrypt(content, passCode);
-//            System.out.println("passCode: " + passCode + " # encryptedStr: " + encryptedStr);
-//            decryptedStr = decrypt(encryptedStr, passCode);
-//            System.out.println("decryptedStr: " + decryptedStr);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-////        System.out.println("securityHelper1:testCodes:contentDe: " + securityHelper1.getContent());
-//        assertTrue(decryptedStr != null);
-//
-//    }
-//
-//    public static String encrypt(String plainText,String key) throws Exception{
-//        String strData="";
-//
-//        try {
-////            String key = "Bar12345Bar12345"; // 128 bit key
-//            String initVector = "RandomInitVector"; // 16 bytes IV
-//            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-//            System.out.println(iv.getIV());
-//            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
-//            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-//            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
-//            byte[] ciphertext = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
-//            strData = new String(ciphertext);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            throw new Exception(e);
-//        }
-//        return strData;
-//    }
-//    public static String decrypt(String strEncrypted,String strKey) throws Exception{
-//        String strData="";
-//
-//        try {
-//            SecretKeySpec keySpec = new SecretKeySpec(strKey.getBytes(StandardCharsets.UTF_8), "AES");
-//            Cipher decoder = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-//            decoder.init(Cipher.DECRYPT_MODE, keyspec);
-//            byte[] decrypted = decoder.doFinal(message);
-//            strData = new String(decrypted);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            throw new Exception(e);
-//        }
-//        return strData;
-//    }
+    // public void testEncryDecry() {
+    // String content = "TestInput";
+    // String decryptedStr;
+    // System.out.println("securityHelper1:testCodesEn:content: " + content);
+    // try {
+    // String passCode = "$W%Ld0os$3$Wdos3";// String key = "Bar12345Bar12345"; //
+    // 128 bit key
+    // String encryptedStr = encrypt(content, passCode);
+    // System.out.println("passCode: " + passCode + " # encryptedStr: " +
+    // encryptedStr);
+    // decryptedStr = decrypt(encryptedStr, passCode);
+    // System.out.println("decryptedStr: " + decryptedStr);
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
+    //// System.out.println("securityHelper1:testCodes:contentDe: " +
+    // securityHelper1.getContent());
+    // assertTrue(decryptedStr != null);
+    //
+    // }
+    //
+    // public static String encrypt(String plainText,String key) throws Exception{
+    // String strData="";
+    //
+    // try {
+    //// String key = "Bar12345Bar12345"; // 128 bit key
+    // String initVector = "RandomInitVector"; // 16 bytes IV
+    // IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+    // System.out.println(iv.getIV());
+    // SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+    // Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+    // cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+    // byte[] ciphertext =
+    // cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+    // strData = new String(ciphertext);
+    //
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // throw new Exception(e);
+    // }
+    // return strData;
+    // }
+    // public static String decrypt(String strEncrypted,String strKey) throws
+    // Exception{
+    // String strData="";
+    //
+    // try {
+    // SecretKeySpec keySpec = new
+    // SecretKeySpec(strKey.getBytes(StandardCharsets.UTF_8), "AES");
+    // Cipher decoder = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+    // decoder.init(Cipher.DECRYPT_MODE, keyspec);
+    // byte[] decrypted = decoder.doFinal(message);
+    // strData = new String(decrypted);
+    //
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // throw new Exception(e);
+    // }
+    // return strData;
+    // }
 }
