bcff7975e58b4c6995d1b039dd71337ba590e05a
Shridhar S D <shridhard@gmail.com>
2023-02-09 11:37:24 +0530

    fixed comments wanted text

--- src/main/java/knt/lib/generic1/StringPadderFactory.java
+++ src/main/java/knt/lib/generic1/StringPadderFactory.java
@@ -1,24 +1,19 @@
 package knt.lib.generic1;
 
 /**
-4
  * Factory for creating instances of {@link StringPadder}.
-5
  */
 public final class StringPadderFactory {
-    private StringPadderFactory() {
-    }
-    /**
-12
-     * Creates an instance of {@link StringPadder}.
-13
-     *
-14
-     * @return the new instance
-15
-     */
-    public static StringPadder createStringPadder() {
-        return new StringPadderImpl();
-    }
+     private StringPadderFactory() {
+     }
+
+     /**
+      * Creates an instance of {@link StringPadder}.
+      *
+      * @return the new instance
+      */
+     public static StringPadder createStringPadder() {
+          return new StringPadderImpl();
+     }
 
 }
