38ded4cd66b1590e5e667352dd1b199c1f9969a8
Shridhar S D <shridhard@gmail.com>
2023-02-09 11:38:00 +0530

    added logic block if (stringToPad == null)

--- src/main/java/knt/lib/generic1/StringPadderImpl.java
+++ src/main/java/knt/lib/generic1/StringPadderImpl.java
@@ -1,36 +1,52 @@
 package knt.lib.generic1;
 
-class StringPadderImpl implements StringPadder {
-    
-        StringPadderImpl() {
-        }
-        @Override
-        public String padLeft(String stringToPad, int totalLength) {
+public class StringPadderImpl implements StringPadder {
+
+    StringPadderImpl() {
+    }
+
+    @Override
+    public String padLeft(String stringToPad, int totalLength) {
+        if (stringToPad == null)
+            return null;
+        else
             return padLeft(stringToPad, totalLength, ' ');
-        }
-    
-        @Override
-    
-        public String padLeft(String stringToPad, int totalLength, char paddingCharacter) {
-    
+    }
+
+    @Override
+
+    public String padLeft(String stringToPad, int totalLength, char paddingCharacter) {
+        if (stringToPad == null)
+            return null;
+        else
             return getStringToBeAdded(stringToPad, totalLength, paddingCharacter) + stringToPad;
-    
-        }
-    
-        @Override
+
+    }
+
+    @Override
     public String padRight(String stringToPad, int totalLength) {
+        if (stringToPad == null)
+            return null;
+        else
             return padRight(stringToPad, totalLength, ' ');
-        }
-        @Override
-        public String padRight(String stringToPad, int totalLength, char paddingCharacter) {
+    }
+
+    @Override
+    public String padRight(String stringToPad, int totalLength, char paddingCharacter) {
+        if (stringToPad == null)
+            return null;
+        else
             return stringToPad + getStringToBeAdded(stringToPad, totalLength, paddingCharacter);
-        }
-        private String getStringToBeAdded(String stringToPad, int totalLength, char paddingCharacter) {
-            int quantity = totalLength - stringToPad.length();
-            if (quantity > 0) {
-                return Character.toString(paddingCharacter).repeat(quantity);
-            } else {
-                return "";
-            }
+    }
+
+    private String getStringToBeAdded(String stringToPad, int totalLength, char paddingCharacter) {
+        if (stringToPad == null)
+            return null;
+        int quantity = totalLength - stringToPad.length();
+        if (quantity > 0) {
+            return Character.toString(paddingCharacter).repeat(quantity);
+        } else {
+            return "";
         }
     }
+}
